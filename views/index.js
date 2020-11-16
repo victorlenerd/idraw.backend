69f47bc0d81dfb1f7d65ace5e7c16a5219194661
Nwaokocha Victor <nvonweb@outlook.com>
2020-11-16 01:35:37 -0500

    client side code

--- views/index.js
+++ views/index.js
@@ -3,12 +3,30 @@
     function main() {
         const baseURL = "https://unilagpastquestions-com.appspot.com/notes/test-note-uuid";
         const imageURL = (fileName) => `https://storage.googleapis.com/idraw-app-images/${fileName}`;
+        const root = document.getElementById("root");
 
-        fetch(baseURL)
-            .then((res) => res.json())
-            .then((noteImages) => {
-                console.log({ noteImages })
-            })
+        setInterval(() => {
+            fetch(baseURL)
+                .then((res) => res.json())
+                .then((noteImages) => {
+                    let maxVersion = Number.MIN_SAFE_INTEGER;
+                    let mostRecentImageUrl = null;
+
+                    noteImages.forEach((noteImage) => {
+                        if (noteImage.version > maxVersion) {
+                            maxVersion = noteImage.version
+                            mostRecentImageUrl = noteImage.file_name;
+                        }
+                    });
+
+                    const image = new Image();
+                    image.src = imageURL(mostRecentImageUrl);
+
+                    root.innerHTML = "";
+
+                    root.appendChild(image);
+                })
+        }, 1000)
     }
 
     window.addEventListener('DOMContentLoaded', () => {
